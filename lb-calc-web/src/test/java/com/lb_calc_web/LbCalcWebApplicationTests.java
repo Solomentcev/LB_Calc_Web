590cd83b33e217351ffcd2b2072e27616b94cdd1
denis <sda-omd@yandex.ru>
2026-09-18 21:54:46 +0300

    Refactor tests to use active profiles and environment-specific configuration

--- lb-calc-web/src/test/java/com/lb_calc_web/LbCalcWebApplicationTests.java
+++ lb-calc-web/src/test/java/com/lb_calc_web/LbCalcWebApplicationTests.java
@@ -2,12 +2,13 @@ package com.lb_calc_web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LbCalcWebApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
