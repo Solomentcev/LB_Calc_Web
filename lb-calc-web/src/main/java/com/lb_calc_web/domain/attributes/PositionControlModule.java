54777a4c0c6b1d43e11f85991a59a4b44dedd209
Denis <sda-omd@yandex.ru>
2026-09-18 20:55:25 +0300

    Add generic control module position

---
+++ lb-calc-web/src/main/java/com/lb_calc_web/domain/attributes/PositionControlModule.java
@@ -0,0 +1,10 @@
+package com.lb_calc_web.domain.attributes;
+
+/**
+ * Положение единственного модуля управления внутри ALS.
+ */
+public enum PositionControlModule {
+    LEFT,
+    CENTER,
+    RIGHT
+}
